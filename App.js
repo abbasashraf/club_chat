9c1495413e99614560f18e4a607f1cb10533d424
talhamuhammadashraf <talhamuhammadashraf@gmail.com>
2018-03-13 09:34:21 +0500

    initial

---
+++ App.js
@@ -0,0 +1,10 @@
+import React ,{Component} from 'react';
+import {StackNavigator,} from 'react-navigation'
+export default class App extends Component{
+  render() {
+    return (
+
+    );
+  }
+}
+
